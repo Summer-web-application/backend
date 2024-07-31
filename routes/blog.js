61af0f3c0a31c9c6117a05d90b8672e7a4d8c636
Kalle Määttä <kalleilmari@icloud.com>
2024-07-31 10:10:07 +0300

    updated dislike

--- routes/blog.js
+++ routes/blog.js
@@ -91,7 +91,23 @@ blogRouter.post("/comment/new", async (req,res) => {
         const sql = `WITH inserted_comment AS(INSERT INTO comments (text, likes, post_id, user_id)
         VALUES ($1, $2, $3, $4) RETURNING *) SELECT inserted_comment.*, users.first_name, users.last_name, users.username
         FROM inserted_comment JOIN users ON inserted_comment.user_id = users.id WHERE inserted_comment.post_id = $3`;
-        const result = await executeQuery(sql, [req.body.text, likes, req.body.postId, req.body.user_id]);
+        const result = await executeQuery(sql, [req.body.text, likes, req.body.post_id, req.body.user_id]);
+        const rows = result.rows ? result.rows : [];
+        console.log(rows , "comment");
+        res.status(200).json(rows);
+    } catch (error) {
+        res.statusMessage = error;
+        res.status(500).json({error: error});
+    }
+})
+
+//get users comment likes !!change to also check the post
+blogRouter.get("/:user_id/:post_id/comments/likes", async (req,res) => {
+    const user_id = parseInt(req.params.user_id);
+    const post_id = parseInt(req.params.post_id);
+    try {
+        const sql = 'SELECT comment_id from user_comment_likes WHERE user_id = $1 AND post_id = $2';
+        const result = await executeQuery(sql, [user_id, post_id]);
         const rows = result.rows ? result.rows : [];
         res.status(200).json(rows);
     } catch (error) {
@@ -100,14 +116,63 @@ blogRouter.post("/comment/new", async (req,res) => {
     }
 })
 
-//get users post likes
+//like and dislike comment
+blogRouter.put("/comment/like", async (req,res) => {
+    console.log("likeStatus: ", req.body.like_status);
+    const likeStatus = req.body.like_status;
+   
+    try {
+        let likeResult
+        if(likeStatus == "like"){
+            likeResult = await likeComment(req.body.user_id, req.body.comment_id, req.body.post_id);
+        } else if (likeStatus == "dislike"){
+            likeResult = await dislikeComment(req.body.comment_id);
+        } else {
+            console.log("like status error");
+        }
+        res.status(200).json(likeResult);
 
+    } catch (error) {
+        res.statusMessage = error;
+        res.status(500).json({error: error});
+    }
+})
 
-//get users comment likes !!change to also check the post
-blogRouter.get("/:id/comments/likes", async (req,res) => {
+async function likeComment(user_id, comment_id, post_id) {
+    try {
+        const sql = 'INSERT INTO user_comment_likes (user_id, comment_id, post_id) VALUES ($1, $2, $3)';
+        const result = await executeQuery(sql, [user_id, comment_id, post_id]);
+
+        if(result.rowCount > 0) {
+            const updateSql = 'UPDATE comments SET likes = likes + 1 WHERE id = $1 RETURNING likes';
+            const updateResult = await executeQuery(updateSql, [comment_id]);
+            const rows = updateResult.rows ? updateResult.rows : [];
+            return rows;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+async function dislikeComment(comment_id) {
+    try {
+        const sql = 'DELETE from user_comment_likes WHERE comment_id = $1';
+        const result = await executeQuery(sql, [comment_id]);
+        console.log(result, "dislike result");
+
+        if(result.rowCount > 0){
+            const updateSql = 'UPDATE comments SET likes = likes - 1 WHERE id = $1 RETURNING likes';
+            const updateResult = await executeQuery(updateSql, [comment_id]);
+            const rows = updateResult.rows ? updateResult.rows : [];
+            return rows;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+blogRouter.get("/:id/posts/likes", async (req,res) => {
     const id = parseInt(req.params.id);
     try {
-        const sql = 'SELECT comment_id from user_comment_likes WHERE user_id = $1';
+        const sql = 'SELECT post_id from user_post_likes WHERE user_id = $1';
         const result = await executeQuery(sql, [id]);
         const rows = result.rows ? result.rows : [];
         res.status(200).json(rows);
@@ -116,39 +181,58 @@ blogRouter.get("/:id/comments/likes", async (req,res) => {
         res.status(500).json({error: error});
     }
 })
-//like or dislike system
-blogRouter.put("/comment/like", async (req,res) => {
-    console.log("likeStatus: ", req.body.likeStatus);
+//like dislike post
+blogRouter.put("/post/like", async (req,res) => {
+    const likeStatus = req.body.like_status;
+    console.log(likeStatus, " Post likestatus");
     try {
-        const sql = 'INSERT INTO user_comment_likes (user_id, comment_id) VALUES ($2, $1)';
-        const result = await executeQuery(sql, [req.body.comment_id, req.body.user_id]);
-
-        if(result.rowCount > 0) {
-            const updateSql = 'UPDATE comments SET likes = likes + 1 WHERE id = $1 RETURNING likes';
-            const updateResult = await executeQuery(updateSql, [req.body.comment_id]);
-            const rows = updateResult.rows ? updateResult.rows : [];
-            res.status(200).json(rows);
+        let likeResult
+        if(likeStatus == "like"){
+            likeResult = await likePost(req.body.user_id, req.body.post_id);
+        } else if (likeStatus == "dislike"){
+            likeResult = await dislikePost(req.body.post_id);
+        } else {
+            console.log("like status error");
         }
+        res.status(200).json(likeResult);
+
     } catch (error) {
         res.statusMessage = error;
         res.status(500).json({error: error});
     }
 })
 
-async function likeComment() {
+async function likePost(user_id, post_id) {
     try {
-        const sql = 'INSERT INTO user_comment_likes (user_id, comment_id, like_status) VALUES ($1, $2)';
-        const result = await executeQuery(sql, [req.body.user_id, req.body.comment_id, req.body.like_status]);
-    } catch (error) {
-        return error;
-    }
-}
-async function dislikeComment() {
-    try {
+        const sql = 'INSERT INTO user_post_likes (user_id, post_id) VALUES ($1, $2)';
+        const result = await executeQuery(sql, [user_id, post_id]);
 
+        if(result.rowCount > 0) {
+            const updateSql = 'UPDATE posts SET likes = likes + 1 WHERE id = $1 RETURNING likes';
+            const updateResult = await executeQuery(updateSql, [post_id]);
+            const rows = updateResult.rows ? updateResult.rows : [];
+            return rows;
+        }
     } catch (error) {
         return error;
     }
 }
+async function dislikePost(post_id) {
+    try {
+        const sql = 'DELETE from user_post_likes WHERE post_id = $1';
+        const result = await executeQuery(sql, [post_id]);
+        console.log(result, "dislike result");
+
+        if(result.rowCount > 0){
+            const updateSql = 'UPDATE posts SET likes = likes - 1 WHERE id = $1 RETURNING likes';
+            const updateResult = await executeQuery(updateSql, [post_id]);
+            const rows = updateResult.rows ? updateResult.rows : [];
+            return rows;
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
 
 module.exports = {blogRouter}
